b9d1c7f55a04ae0afdb9363d5704ef8c66dea922
Nicolas Agostini <n.b.agostini@gmail.com>
2020-05-14 18:33:09 -0400

    Get authtoken

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,11 +15,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 from core.views import TestView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', TestView.as_view(), name='test')
+    path('', TestView.as_view(), name='test'),
+    path('api/token/', obtain_auth_token, name='obtain-token')
 ]
+
+# The following code could be use to post a request to this api in order to
+# get the rest authtoken for the user
+"""
+import requests
+url = "http://<ip>/api/token/"
+
+payload = {'username': '<user>',
+           'password': '<passwd>'}
+files = [
+]
+headers = {}
+
+response = requests.request(
+    "POST", url, headers=headers, data=payload, files=files)
+
+print(response.text.encode('utf8'))
+"""
