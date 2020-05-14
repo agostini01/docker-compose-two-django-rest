17930bca3f32dd1b48b4c44f3bbec8896c8ec06f
Nicolas Agostini <n.b.agostini@gmail.com>
2020-05-14 16:13:29 -0400

    Create a rest serializer for the model

--- api/core/views.py
+++ api/core/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from .serializers import PostSerializer
+from .models import Post
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
@@ -13,3 +12,10 @@ class TestView(APIView):
                 }
 
         return Response(data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
