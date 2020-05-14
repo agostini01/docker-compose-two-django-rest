5eea495ef6e62422b2e8f1d0dfda347cfce169a0
Nicolas Agostini <n.b.agostini@gmail.com>
2020-05-14 18:23:57 -0400

    Implement rest auth token

--- api/api/settings.py
+++ api/api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Third party
     'rest_framework',
+    'rest_framework.authtoken',
 
     # Local
     'core',
@@ -124,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
