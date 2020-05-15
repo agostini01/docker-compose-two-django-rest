342a2aff5f61ca9b306c26aafb9559e3f4c40699
Nicolas Agostini <n.b.agostini@gmail.com>
2020-05-15 12:14:24 -0400

    Create the post to API functionality

--- app/pages/views.py
+++ app/pages/views.py
@@ -49,7 +49,7 @@ class PostView(FormView):
     template_name = 'pages/post.html'
 
     form_class = CreatePostForm
-    success_url = ''
+    success_url = '.'
 
     def get_context_data(self, **kwargs):
         context = super(PostView, self).get_context_data(**kwargs)
@@ -60,18 +60,24 @@ class PostView(FormView):
         # This method is called when valid form data has been POSTed.
 
         # Proccess the form to send to the Rest API on the api:8888 container
-        print(form.cleaned_data['your_name'])
+        print(form.cleaned_data['title'])
 
         url = "http://api:8888"
 
-        payload = {}
+        payload = {'description': form.cleaned_data['description'],
+                   'title': form.cleaned_data['title']
+                   }
+        files = [
+
+        ]
         headers = {
         }
 
         # In order to use a validation token:
         # https://github.com/PwnSauceDesigns/fundraiser_app/blob/master/fundraiser_app/views.py
         # https://stackoverflow.com/questions/55500246/django-send-a-post-request-through-form-to-another-server/55502006
-        response = requests.request("GET", url, headers=headers, data=payload)
+        response = requests.request(
+            "POST", url, headers=headers, data=payload, files=files)
 
         print(response.text.encode('utf8'))
         print(response.json())
